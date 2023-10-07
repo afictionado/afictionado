ee0e1b4489dba661499e971d28b0df54111f0e01
Viet Duong <ramendev2009@gmail.com>
2023-10-07 19:51:42 +0700

    FEATURE: Set up PostCSS and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				linen: '#F3E9DC',
+				caramel: '#C08552',
+				'caput-mortuum': '#5E3023',
+				'raw-umber': '#895737',
+				'desert-sand': '#DAB49D'
+			}
+		}
+	},
+	plugins: []
+};
