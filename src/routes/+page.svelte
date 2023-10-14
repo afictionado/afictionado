1f4ea1d4031fd709f9f9f605220108a96bffb7f5
Viet Duong <ramendev2009@gmail.com>
2023-10-14 17:06:36 +0700

    REMOVAL: Rethink what content to put on the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import "../app.pcss";
-	import HomeForBanner from "$lib/components/landing/HomeForBanner.svelte";
 </script>
 
 <svelte:head>
 	<title>Afictionado! ✍️</title>
 </svelte:head>
-
-<HomeForBanner />
