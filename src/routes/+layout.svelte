44eed7c5e4113e4b9bda075d18fd02ace04a6f79
Viet Duong <ramendev2009@gmail.com>
2023-11-20 18:30:34 +0700

    PROGRESS: Build landing page UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import '../app.postcss';
-
-	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { AppShell, storePopup } from '@skeletonlabs/skeleton';
+
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="sidebarLeft">
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/"><img
+					src="/assets/afictionado-icon.svg"
+					alt="app icon" /></a></li>
+				<li><a href="/home">Home</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</nav>
+	</svelte:fragment>
+</AppShell>
 <slot />
